bd2b7974d9cbec8274bc381002eac1a450595f06
AimenYaseen <aimenyaseen6@gmail.com>
2024-09-09 16:51:17 +0500

    apply dry rule to set student variable

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: %i[show edit update destroy]
+
     def index
         @students = Student.all
     end
@@ -17,15 +19,12 @@ class StudentsController < ApplicationController
     end
 
     def show
-        @student = Student.find(params[:id])
     end
 
     def edit
-        @student = Student.find(params[:id])
     end
 
     def update
-        @student = Student.find(params[:id])
         if @student.update(student_params)
             # redirect_to students_path  # redirect to students listing page
             redirect_to student_path(@student)  # redirect to student details page
@@ -35,7 +34,6 @@ class StudentsController < ApplicationController
     end
    
     def destroy
-        @student = Student.find(params[:id])
         @student.destroy
         redirect_to students_path
     end
@@ -43,5 +41,9 @@ class StudentsController < ApplicationController
     private
     def student_params
         params.require(:student).permit(:first_name, :last_name, :email)
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
     end
 end
